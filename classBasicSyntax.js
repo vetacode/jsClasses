5915b0f695e66b48b506ac14e7ea3086b6608d3c
vetacode <vetacode@gmail.com>
2025-11-26 13:17:00 +0700

    adding parameters on sayHi function constructor

--- classBasicSyntax.js
+++ classBasicSyntax.js
@@ -27,6 +27,7 @@ class User {
 // Usage:
 let user = new User('John');
 user.sayHi();
+console.log(User.prototype.constructor == user.__proto__.constructor);
 
 class User2 {
   constructor(name) {
@@ -42,10 +43,28 @@ console.log(typeof User2); // function
 
 // ...or, more precisely, the constructor method
 console.log(User2 === User2.prototype.constructor); // true
-console.log(User2 === Function); // true
 
 // The methods are in User2.prototype, e.g:
 console.log(User2.prototype.sayHi); // the code of the sayHi method
 
 // there are exactly two methods in the prototype
 console.log(Object.getOwnPropertyNames(User.prototype)); // constructor, sayHi
+
+function Car() {}
+Car.prototype.sayHi = function () {};
+console.log(Car.prototype.constructor == Car); //true
+
+class User3 {
+  constructor(jeneng) {
+    this.jeneng = jeneng;
+  }
+
+  sayHi(pesan) {
+    return `${this.jeneng} ${pesan}`;
+  }
+}
+
+const user3 = new User3('Veta');
+console.log(user3.constructor === User3); // true
+console.log(user3.jeneng); //Veta
+console.log(user3.sayHi('mau makan')); //Veta
