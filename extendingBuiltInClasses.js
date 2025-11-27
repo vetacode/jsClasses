f7cce2e9c7cb58fca815509624b374080b38c4c7
vetacode <vetacode@gmail.com>
2025-11-27 22:55:27 +0700

    adding [Symbol.species]

--- extendingBuiltInClasses.js
+++ extendingBuiltInClasses.js
@@ -12,3 +12,26 @@ console.log(arr.isEmpty()); // false
 let filteredArr = arr.filter((item) => item >= 10);
 console.log(filteredArr); // 10, 50
 console.log(filteredArr.isEmpty()); // false
+
+{
+  //filter, map, set are using [Symbol.secies] to return to Array (parent Obj)
+  class PowerArray extends Array {
+    isEmpty() {
+      return this.length === 0;
+    }
+
+    // built-in methods will use this as the constructor
+    static get [Symbol.species]() {
+      return Array;
+    }
+  }
+
+  let arr = new PowerArray(1, 2, 5, 10, 50);
+  console.log(arr.isEmpty()); // false
+
+  // filter creates new array using arr.constructor[Symbol.species] as constructor
+  let filteredArr = arr.filter((item) => item >= 10);
+
+  // filteredArr is not PowerArray, but Array
+  // console.log(filteredArr.isEmpty()); // Error: filteredArr.isEmpty is not a function
+}
