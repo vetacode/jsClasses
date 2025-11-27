6536904650db8bad58494981bbadcb36b7ef2c38
vetacode <vetacode@gmail.com>
2025-11-27 22:33:43 +0700

    adding read-only

--- privateProtectedPropsMethods.js
+++ privateProtectedPropsMethods.js
@@ -27,3 +27,26 @@ console.log(coffeeMachine);
 // add water
 coffeeMachine.waterAmount = -10; // _waterAmount will become 0, not -10
 console.log(coffeeMachine.waterAmount); // 0
+
+{
+  //READ ONLY. 'power'
+  class CoffeeMachine {
+    // ...
+
+    constructor(power) {
+      this._power = power;
+    }
+
+    get power() {
+      return this._power;
+    }
+  }
+
+  // create the coffee machine
+  let coffeeMachine = new CoffeeMachine(100);
+
+  console.log(`Power is: ${coffeeMachine.power}W`); // Power is: 100W
+
+  coffeeMachine.power = 25; // Error (no setter)
+  console.log(coffeeMachine.power); //100
+}
