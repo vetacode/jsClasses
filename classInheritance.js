85276c9d1163f4e6fb71000ebfe304b32101c5a7
vetacode <vetacode@gmail.com>
2025-11-27 17:18:45 +0700

    adding [[HomeObject]]

--- classInheritance.js
+++ classInheritance.js
@@ -151,5 +151,33 @@
 
 {
   //Super: internals, [[HomeObject]]
-  //ADVANCED NOTES:
+  //ADVANCED NOTES: It’s about the internal mechanisms behind inheritance and super.
+  let animal = {
+    name: 'Animal',
+    eat() {
+      // animal.eat.[[HomeObject]] == animal
+      alert(`${this.name} eats.`);
+    },
+  };
+
+  let rabbit = {
+    __proto__: animal,
+    name: 'Rabbit',
+    eat() {
+      // rabbit.eat.[[HomeObject]] == rabbit
+      super.eat();
+    },
+  };
+
+  let longEar = {
+    __proto__: rabbit,
+    name: 'Long Ear',
+    eat() {
+      // longEar.eat.[[HomeObject]] == longEar
+      super.eat();
+    },
+  };
+
+  // works correctly
+  longEar.eat(); // Long Ear eats.
 }
