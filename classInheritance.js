06ff0e1395f06fae863a28ab79d9d88e271cdbdb
vetacode <vetacode@gmail.com>
2025-11-27 16:13:26 +0700

    addin extends keyword

---
+++ classInheritance.js
@@ -0,0 +1,33 @@
+//EXTENDS Keyword
+//The syntax to extend another class is: class Child extends Parent.
+{
+  class Animal {
+    constructor(name) {
+      this.name = name;
+      this.speed = 0;
+    }
+    run(speed) {
+      this.speed = speed;
+      console.log(`${this.name} runs with speed ${this.speed} km/h`);
+    }
+
+    stop() {
+      this.speed = 0;
+      console.log(`${this.name} stands still`);
+    }
+  }
+  let animal = new Animal('My Animal');
+  animal.stop();
+  animal.run(50);
+
+  class Rabbit extends Animal {
+    hide() {
+      console.log(`${this.name} hides!`);
+    }
+  }
+
+  let rabbit = new Rabbit('Long Ears');
+
+  rabbit.run(30);
+  rabbit.hide();
+}
